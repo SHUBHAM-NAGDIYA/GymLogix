a71c1d6991d04ad3f70d2bfa9c92cc0c63ffcf9d
SHUBHAM-NAGDIYA <143119328+SHUBHAM-NAGDIYA@users.noreply.github.com>
2025-05-28 23:22:31 +0530

    Fix settings module path and update wsgi.py for deployment

--- GymLogix/GymLogix/wsgi.py
+++ GymLogix/GymLogix/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add the project root to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# Set the correct settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GymLogix.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
