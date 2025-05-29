50a666116bff6d985d5d0cfd32507a9e9284765a
SHUBHAM-NAGDIYA <143119328+SHUBHAM-NAGDIYA@users.noreply.github.com>
2025-05-29 18:13:33 +0530

    Update settings.py

--- GymLogix/GymLogix/settings.py
+++ GymLogix/GymLogix/settings.py
@@ -132,3 +132,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'GymLogix','static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
